1931685c2d109bf31eae5c55588c421fc6a2b2c2
gaeunpark7 <hen77360@gmail.com>
2026-04-21 15:51:05 +0900

    feat: DB 연동을 위한 repo impl 수정

--- flutter_clean_architecture/note_app/lib/data/repository/note_repository_impl.dart
+++ flutter_clean_architecture/note_app/lib/data/repository/note_repository_impl.dart
@@ -8,32 +8,27 @@ class NoteRepositoryImpl implements NoteRepository {
   NoteRepositoryImpl({required this.db});
 
   @override
-  Future<void> deleteNote(Note note) {
-    // TODO: implement deleteNote
-    throw UnimplementedError();
+  Future<void> deleteNote(Note note) async {
+    await db.deleteNote(note);
   }
 
   @override
-  Future<List<Note>> getNote() {
-    // TODO: implement getNote
-    throw UnimplementedError();
+  Future<Note?> getNoteById(int id) async {
+    return await db.getNoteById(id);
   }
 
   @override
-  Future<Note> getNoteById(int id) {
-    // TODO: implement getNoteById
-    throw UnimplementedError();
+  Future<List<Note>> getNote() async {
+    return await db.getNotes();
   }
 
   @override
-  Future<void> insertNote(Note note) {
-    // TODO: implement insertNote
-    throw UnimplementedError();
+  Future<void> insertNote(Note note) async {
+    await db.insertNote(note);
   }
 
   @override
-  Future<void> updateNote(Note note) {
-    // TODO: implement updateNote
-    throw UnimplementedError();
+  Future<void> updateNote(Note note) async {
+    await db.updateNote(note);
   }
 }
--- flutter_clean_architecture/note_app/lib/domain/repository/note_repository.dart
+++ flutter_clean_architecture/note_app/lib/domain/repository/note_repository.dart
@@ -3,7 +3,7 @@ import 'package:note_app/domain/model/note.dart';
 abstract class NoteRepository {
   Future<List<Note>> getNote();
 
-  Future<Note> getNoteById(int id);
+  Future<Note?> getNoteById(int id);
 
   Future<void> insertNote(Note note);
 
